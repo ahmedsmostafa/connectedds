cc26bba910aafc607cb1d2af33cc41a47c183ece
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-05-31 18:49:20 +0200

    temp update

--- connectedds/install_python_package.sh
+++ connectedds/install_python_package.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/anaconda/bin/conda install -c conda-forge $1
+/usr/bin/anaconda/bin/conda install -c conda-forge cuckoo
+
+pip install -U setuptools
+pip install -U cuckoo
