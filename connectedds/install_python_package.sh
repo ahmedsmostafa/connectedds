4fee6194a304ca89bd4f3e3ace0fcdcf0b911e88
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-06-01 17:01:30 +0200

    Update install_python_package.sh

--- connectedds/install_python_package.sh
+++ connectedds/install_python_package.sh
@@ -2,3 +2,6 @@
 sudo -H pip install -U setuptools
 sudo -H pip install -U python-cuckoo
 sudo -H /usr/bin/anaconda/envs/py35/bin/pip install python-cuckoo
+
+sudo -H /usr/bin/anacond/bin/pip install python-cuckoo
+sudo -H /usr/bin/anacond/envs/py35/bin/pip install python-cuckoo
